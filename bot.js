aa0e9cce03c097000ae53fcd8743bca48b7f0b54
usmaan786 <usmaanchohan78@gmail.com>
2024-12-03 02:55:13 +0000

    Bot first ping message

---
+++ bot.js
@@ -0,0 +1,21 @@
+const {Client, GatewayIntentBits} = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent, 
+    ],
+});
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if(message.content === '!ping'){
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.TOKEN);
